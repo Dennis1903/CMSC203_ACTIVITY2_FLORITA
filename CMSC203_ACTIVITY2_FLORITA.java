979d1a0fdebc852c9e3bd61aa310ad42f92fd462
Dennis1903 <112455243+Dennis1903@users.noreply.github.com>
2022-10-15 04:10:35 -0700

    Add files via upload

---
+++ CMSC203_ACTIVITY2_FLORITA.java
@@ -0,0 +1,27 @@
+package CMSC203_ACTIVITY2_FLORITA;
+
+import java.util.Scanner;
+
+public class CMSC203_ACTIVITY2_FLORITA {
+	
+	public static void main(String[] args) {
+		
+		float numOne;
+		float numTwo;
+		float result;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("First Number : ");
+		numOne = s.nextFloat();
+		
+		System.out.print("Second Number : ");
+		numTwo = s.nextFloat();
+		
+		result = numOne + numTwo;
+		
+		System.out.println();
+		System.out.println(numOne + " + " + numTwo + " = " + result);
+	}
+
+}
